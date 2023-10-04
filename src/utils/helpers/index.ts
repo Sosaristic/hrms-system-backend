52b6333bef94a4dcbec02b2683e6c89969cf5204
Theedon <faniyantoyin@gmail.com>
2023-10-04 08:18:55 +0100

    fix: correct all code formatting issues on files from initial commit

--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,21 +1,23 @@
-import crypto from 'crypto';
+import crypto from "crypto";
 
 export const generateRandomString = (length: number) => {
-  return crypto.randomBytes(length).toString('hex');
+  return crypto.randomBytes(length).toString("hex");
 };
 
 export const generateHash = (password: string, salt: string) => {
-  return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+  return crypto.pbkdf2Sync(password, salt, 1000, 64, "sha512").toString("hex");
 };
 
 export const generateSalt = () => {
-  return crypto.randomBytes(16).toString('hex');
+  return crypto.randomBytes(16).toString("hex");
 };
 
 export const generateSessionToken = () => {
-  return crypto.randomBytes(64).toString('hex');
+  return crypto.randomBytes(64).toString("hex");
 };
 
 export const authentication = (salt: string, password: string) => {
-    return crypto.createHmac('sha256', [salt, password].join('/')).update(process.env.SECRET);
-}
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(process.env.SECRET);
+};
