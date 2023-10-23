4bb6c2a77c82492d34fd5b0a9aea79725610f03d
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-10-23 18:19:51 +0100

    commit

--- dist/utils/tryCatch.d.ts
+++ dist/utils/tryCatch.d.ts
@@ -1,2 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-export declare const tryCatch: (controller: (req: Request, res: Response) => Promise<Response>) => (req: Request, res: Response, next: NextFunction) => Promise<void | Response<any, Record<string, any>>>;
+export declare const tryCatch: (
+  controller: (req: Request, res: Response) => Promise<Response>,
+) => (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<void | Response<any, Record<string, any>>>;
